24556202806cb9be4cef5e6ce38eb369a1c1ee6b
zengdm <zdm476106017@qq.com>
2025-03-22 17:14:39 +0900

    docs

--- games/src/data/games.js
+++ games/src/data/games.js
@@ -2,21 +2,21 @@ export default [
     {
         name: 'snake',
         title: 'スネークゲーム',
-        thumbnail: '/assets/snake.webp',
+        thumbnail: './assets/snake.webp',
     },
     {
         name: 'tetris',
         title: 'テトリス',
-        thumbnail: '/assets/tetris.webp',
+        thumbnail: './assets/tetris.webp',
     },
     {
         name: 'minesweeper',
         title: 'マインスイーパー',
-        thumbnail: '/assets/minesweeper.webp', // 可以用临时图
+        thumbnail: './assets/minesweeper.webp', // 可以用临时图
     },
     {
         name: 'warehouse',
         title: '倉庫管理',
-        thumbnail: '/assets/warehouse.webp', // 可以用临时图
+        thumbnail: './assets/warehouse.webp', // 可以用临时图
     }
 ]
