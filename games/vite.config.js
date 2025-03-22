3238918a52b94b5a4952d69a87acd1646d55b1c4
zengdm <zdm476106017@qq.com>
2025-03-22 17:18:42 +0900

    docs

--- games/vite.config.js
+++ games/vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/gamesDevelopedByAIsHelp/",
+export default {
+  base: '/gamesDevelopedByAIsHelp/',
   build: {
     outDir: "../docs"
   },
@@ -19,4 +19,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}
